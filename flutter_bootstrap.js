26a8979bf172f401fdb6ac49271f9cd8c2661e83
Sara Biavaschi <103214669+sarabiava@users.noreply.github.com>
2025-07-04 22:25:47 +0200

    Image cropper screen.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2411874452"
+    serviceWorkerVersion: "3873927664"
   }
 });
